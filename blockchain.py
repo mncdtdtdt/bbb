4b9c3a46a60c77e62cdd7d122b6b2fff0be1b56d
Mokaram <39893026+mncdtdtdt@users.noreply.github.com>
2024-05-09 23:05:00 +0300

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -522,5 +522,7 @@ def new_transaction():
         return jsonify(response), 201
 
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
