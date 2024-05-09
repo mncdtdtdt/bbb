6a58876d59cb49b5f929330b53d598329d756c3e
Mokaram <39893026+mncdtdtdt@users.noreply.github.com>
2024-05-09 21:42:11 +0300

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -525,7 +525,7 @@ def new_transaction():
 
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
     db.create_all()
     print('Created Database!')
         
